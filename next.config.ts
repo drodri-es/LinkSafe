ca9eed091f89eff12e2c7085183c178143c59f4b
David Rodriguez <drodri@gmail.com>
2025-07-22 09:51:28 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -58,6 +58,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'www.amstrad.es',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
