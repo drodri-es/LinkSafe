8709234cc6f6a2aec4cc0ea8d69323acc0bb65aa
David Rodriguez <drodri@gmail.com>
2025-07-22 11:33:00 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -64,6 +64,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'es.wikipedia.org',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
